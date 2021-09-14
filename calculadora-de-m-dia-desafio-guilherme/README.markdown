d834789a27cdc0961a55a05861cc0fa151150126
xleomachado <89798277+xleomachado@users.noreply.github.com>
2021-09-14 19:02:10 -0300

    Add files via upload

---
+++ calculadora-de-m-dia-desafio-guilherme/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média (desafio guilherme)
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/xleomachado/pen/VwWMbKg](https://codepen.io/xleomachado/pen/VwWMbKg).
+
+
---
+++ calculadora-de-m-dia-desafio-guilherme/dist/script.js
@@ -0,0 +1,15 @@
+var nome = "Leo"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 8
+var notaDoQuartoBimestre = 8
+
+var notaSomada = notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre 
+var media = notaSomada / 4
+ 
+var notaFixada = media.toFixed(4)
+
+console.log("Bem vindo leo, sua média é:")
+console.log(media)
+
+//fazer menssagem de aprovação e reprovação
