2f7b401d78c1d7ab43fc777f467442dc45592eea
xleomachado <89798277+xleomachado@users.noreply.github.com>
2021-09-14 19:00:26 -0300

    Add files via upload

---
+++ calculadora-de-m-dia/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/xleomachado/pen/mdwBpva](https://codepen.io/xleomachado/pen/mdwBpva).
+
+
---
+++ calculadora-de-m-dia/src/script.js
@@ -0,0 +1,14 @@
+var nome = "Leo"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 8
+var notaDoQuartoBimestre = 8
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+ 
+var notaFixada = notaFinal.toFixed(4)
+
+console.log("Bem vindo leo, sua média é:")
+console.log(notaFinal)
+
