7929d0b87e9953744e9b30286127bf644f370ab7
xleomachado <89798277+xleomachado@users.noreply.github.com>
2021-09-14 19:01:49 -0300

    Add files via upload

---
+++ calculadora-de-m-dia-desafio-rafaella/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média (desafio Rafaella)
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/xleomachado/pen/mdwMqYL](https://codepen.io/xleomachado/pen/mdwMqYL).
+
+
---
+++ calculadora-de-m-dia-desafio-rafaella/src/script.js
@@ -0,0 +1,13 @@
+var nome = "Leo"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+ 
+var notaFixada = notaFinal.toFixed(4)
+
+console.log("Bem vindo leo, sua média é")
+console.log((9 + 7 + 4 + 2) / 4)
+
